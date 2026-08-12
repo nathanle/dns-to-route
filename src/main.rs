189d7f3ef86cdac21b94d0592d13f7ace1b06e7d
Nathan <nlesueur@gmail.com>
2026-08-12 21:20:18 +0000

    Added deletion, need to cleanup adds so that we do not waste time adding what is there.:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-use std::{env, net::Ipv4Addr};
-use hickory_resolver::Resolver;
+use std::{env, net::Ipv4Addr, net::IpAddr};
+use hickory_resolver::{Resolver, lookup_ip::LookupIp};
 
 use futures_util::TryStreamExt;
 use ipnetwork::Ipv4Network;
-use rtnetlink::{new_connection, Error, Handle, RouteMessageBuilder, IpVersion};
-use netlink_packet_route::route::{RouteMessage, RouteProtocol};
+use rtnetlink::{new_connection, Error, Handle, RouteMessageBuilder};
+use netlink_packet_route::route::{RouteAddress, RouteAttribute, RouteMessage, RouteProtocol};
 
 
 const DNS_ROUTE: RouteProtocol = RouteProtocol::Other(42); 
@@ -15,15 +15,6 @@ const DNS_ROUTE: RouteProtocol = RouteProtocol::Other(42);
 async fn main() -> Result<(), ()> {
     let (connection, handle, _) = new_connection().unwrap();
     tokio::spawn(connection);
-    let current_routes = RouteMessageBuilder::<Ipv4Addr>::new()
-        .build();
-    let mut routes = handle.route().get(current_routes).execute();
-
-    while let Ok(Some(route)) = routes.try_next().await {
-        if matches_protocol(&route, RouteProtocol::Babel) {
-            process_route(route);
-        }
-    }
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
         println!("Argument count: {}", args.len());
@@ -38,12 +29,43 @@ async fn main() -> Result<(), ()> {
         eprintln!("invalid interface");
         std::process::exit(1);
     });
+    let iface = iface.into();
+    let iface_idx = handle
+        .link()
+        .get()
+        .match_name(iface)
+        .execute()
+        .try_next()
+        .await
+        .unwrap()
+        .unwrap()
+        .header
+        .index;
     let source: Ipv4Addr = args[3].parse().unwrap_or_else(|_| {
         eprintln!("invalid source");
         std::process::exit(1);
     });
     let resolver = Resolver::builder_tokio().unwrap().build().unwrap();
     let response = resolver.lookup_ip(dnsname).await.unwrap();
+    let current_routes = RouteMessageBuilder::<Ipv4Addr>::new()
+        .build();
+    let routes_stream = handle.route().get(current_routes).execute();
+    let routes = routes_stream.try_collect::<Vec<_>>().await.unwrap();
+    //while let Ok(Some(route)) = routes_stream.try_next().await {
+        //let test = route.attributes
+        //    .iter()
+        //    .find(|item| {
+        //        matches!(item, RouteAttribute::Destination(RouteAddress::Inet(_)))
+        //    });
+        //println!("{:?}", test);
+
+    for x in &routes {
+        if matches_protocol(&x, RouteProtocol::Babel) {
+            if process_route(handle.clone(), x.clone(), response.clone()).await {
+
+            }
+        }
+    }
     for a in response.iter() {
         if a.is_ipv4() {
             let dest: Ipv4Network = format!("{}/32", a).parse().unwrap_or_else(|_| {
@@ -51,7 +73,7 @@ async fn main() -> Result<(), ()> {
                 std::process::exit(1);
             });
             println!("{}", a);
-            if let Err(e) = add_route(&dest, &iface, source, handle.clone()).await {
+            if let Err(e) = add_route(&dest, iface_idx, source, handle.clone(), a).await {
                 eprintln!("{e}");
             };
         }
@@ -61,23 +83,13 @@ async fn main() -> Result<(), ()> {
 
 async fn add_route(
     dest: &Ipv4Network,
-    iface: impl Into<String>,
+    iface_idx: u32,
     source: Ipv4Addr,
     handle: Handle,
+    address: IpAddr,
 ) -> Result<(), Error> {
-    let iface = iface.into();
-    let iface_idx = handle
-        .link()
-        .get()
-        .match_name(iface)
-        .execute()
-        .try_next()
-        .await?
-        .unwrap()
-        .header
-        .index;
 
-    println!("{}", "Adding route!");
+    println!("Adding route: {}", address);
     let route = RouteMessageBuilder::<Ipv4Addr>::new()
         .destination_prefix(dest.ip(), dest.prefix())
         .output_interface(iface_idx)
@@ -95,8 +107,34 @@ fn matches_protocol(route: &RouteMessage, target: RouteProtocol) -> bool {
     false
 }
 
-fn process_route(route: RouteMessage) {
-    println!("Found route: {:#?}", route);
+async fn process_route(
+    handle: Handle,
+    route: RouteMessage,
+    response: LookupIp
+) -> bool {
+    for attribute in route.attributes.iter() {
+        if let RouteAttribute::Destination(route_addr) = attribute {
+            let dest_ip: IpAddr = match route_addr {
+                RouteAddress::Inet(ipv4) => IpAddr::V4(*ipv4),
+                _ => continue,
+            };
+            if response.iter().any(|x| x == dest_ip) {
+                println!("Route for {} exists. No modifications needed.", dest_ip);
+                return false 
+            } else {
+                println!("I am deleting the route for {:?} as it is not in DNS results", dest_ip);
+                let _ = handle
+                    .route()
+                    .del(route)
+                    .execute()
+                    .await
+                    .map_err(|e| format!("Failed to delete route: {}", e));
+                return false
+            }
+            
+        }
+    }
+    return true 
 }
 
 
